8e97875bae15747f0ba0a62eaae18235e05897c1
Pioneer-wxh <151451952+Pioneer-wxh@users.noreply.github.com>
2025-06-29 15:11:27 +0800

    Update eval.py

--- CR_MR/scripts_for_MR/eval.py
+++ CR_MR/scripts_for_MR/eval.py
@@ -5,44 +5,47 @@
 # $3: 输出目录
 # $4: 使用的 GPU 设备号
 
-CUDA_VISIBLE_DEVICES=$4 python math_evaluate.py \
+
+
+CUDA_VISIBLE_DEVICES=$2 python math_evaluate.py \
     --model Qwen2.5-7B-Instruct \
     --adapter LoRA \
     --dataset gsm8k \
     --base_model 'Qwen/Qwen2.5-7B-Instruct' \
-    --lora_weights $3|tee -a $3/gsm8k.txt
+    --lora_weights $1|tee -a $1/gsm8k.txt
 
-CUDA_VISIBLE_DEVICES=$4 python math_evaluate.py \
+CUDA_VISIBLE_DEVICES=$2 python math_evaluate.py \
     --model Qwen2.5-7B-Instruct \
     --adapter LoRA \
     --dataset AQuA \
     --base_model 'Qwen/Qwen2.5-7B-Instruct' \
-    --lora_weights $3|tee -a $3/AQuA.txt
+    --lora_weights $1|tee -a $1/AQuA.txt
 
-CUDA_VISIBLE_DEVICES=$4 python math_evaluate.py \
+CUDA_VISIBLE_DEVICES=$2 python math_evaluate.py \
     --model Qwen2.5-7B-Instruct \
     --adapter LoRA \
     --dataset MultiArith \
     --base_model 'Qwen/Qwen2.5-7B-Instruct' \
-    --lora_weights $3|tee -a $3/MultiArith.txt
+    --lora_weights $1|tee -a $1/MultiArith.txt
 
-CUDA_VISIBLE_DEVICES=$4 python math_evaluate.py \
+CUDA_VISIBLE_DEVICES=$2 python math_evaluate.py \
     --model Qwen2.5-7B-Instruct \
     --adapter LoRA \
     --dataset SVAMP \
     --base_model 'Qwen/Qwen2.5-7B-Instruct' \
-    --lora_weights $3|tee -a $3/SVAMP.txt
+    --lora_weights $1|tee -a $1/SVAMP.txt
 
-CUDA_VISIBLE_DEVICES=$4 python math_evaluate.py \
+CUDA_VISIBLE_DEVICES=$2 python math_evaluate.py \
     --model Qwen2.5-7B-Instruct \
     --adapter LoRA \
     --dataset SingleEq \
     --base_model 'Qwen/Qwen2.5-7B-Instruct' \
-    --lora_weights $3|tee -a $3/SingleEq.txt
+    --lora_weights $1|tee -a $1/SingleEq.txt
 
-CUDA_VISIBLE_DEVICES=$4 python math_evaluate.py \
+CUDA_VISIBLE_DEVICES=$2 python math_evaluate.py \
     --model Qwen2.5-7B-Instruct \
     --adapter LoRA \
     --dataset AddSub \
     --base_model 'Qwen/Qwen2.5-7B-Instruct' \
-    --lora_weights $3|tee -a $3/AddSub.txt
+    --lora_weights $1|tee -a $1/AddSub.txt
+
